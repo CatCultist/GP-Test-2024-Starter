3cd8ee3119bfa525b217b8fa5a9eb4ce7842331e
Darragh O Dowd <infinitesnakes@gmail.com>
2024-05-02 14:04:41 +0100

    insert --
    git-add

--- Starter Code/root1.gd
+++ Starter Code/root1.gd
@@ -10,5 +10,6 @@ func _ready():
 func _input(event):
 	if event is InputEventKey:
 		var key_event = event as InputEventKey
+		
 		if key_event.keycode == KEY_Q and key_event.pressed:
 			get_tree().quit()
